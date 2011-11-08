d7819d03593e0e1b4655e8ace9f84d5b2b99c2b6
Keegan McAllister <mcallister.keegan@gmail.com>
2011-11-08 11:03:54 -0500

    abstract out option validation

---
+++ Text/RE2/Internal.hs
@@ -0,0 +1,13 @@
+module Text.RE2.Internal
+    ( forceUTF8
+    ) where
+
+import Text.RE2.Types
+
+import qualified Data.ByteString as BS
+
+forceUTF8 :: String -> [Option] -> Either Error [Option]
+forceUTF8 fun_name opts = mapM_ check opts >> return (Encoding UTF8 : opts) where
+    check (Encoding _) = Left (Error Nothing msg BS.empty) where
+        msg = fun_name ++ ": cannot specify Encoding option"
+    check _ = Right ()
--- Text/RE2/Text.hs
+++ Text/RE2/Text.hs
@@ -7,19 +7,17 @@ module Text.RE2.Text
     ) where
 
 import Text.RE2.Types
+import Text.RE2.Internal   ( forceUTF8  )
 import Text.RE2.ByteString ( RE2, stats )
 import qualified Text.RE2.ByteString as B
 
-import qualified Data.ByteString    as BS
 import qualified Data.Text          as T
 import qualified Data.Text.Encoding as T
 
 compile :: [Option] -> T.Text -> Either Error RE2
-compile opts txt = mapM_ check opts >> go where
-    check (Encoding _) = Left (Error Nothing msg BS.empty) where
-        msg = "Text.RE2.Text.compile: cannot specify Encoding option"
-    check _ = Right ()
-    go = B.compile (Encoding UTF8 : opts) (T.encodeUtf8 txt)
+compile opts txt = do
+    newopts <- forceUTF8 "Text.RE2.Text.compile" opts
+    B.compile newopts (T.encodeUtf8 txt)
 
 match :: MatchOptions -> RE2 -> T.Text -> Maybe (Match T.Text)
 match opts re txt = fmap (fmap T.decodeUtf8) . B.match opts re $ T.encodeUtf8 txt
