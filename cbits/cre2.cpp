9bc55a571185d062ff67ffa10ed6253b3400e3ba
Keegan McAllister <mcallister.keegan@gmail.com>
2011-11-08 02:29:11 -0500

    more

--- cbits/cre2.cpp
+++ cbits/cre2.cpp
@@ -1,20 +1,22 @@
+#include <string>
+
 #include <re2/re2.h>
 #include "cre2.h"
 
-#define OPT(opt) (reinterpret_cast<RE2::Options *>(opt))
+#define TO_OPT(opt) (reinterpret_cast<RE2::Options *>(opt))
 
 cre2_options *cre2_opt_new(void) {
     return reinterpret_cast<void*>(new RE2::Options());
 }
 
 void cre2_opt_delete(cre2_options *opt) {
-    delete OPT(opt);
+    delete TO_OPT(opt);
 }
 
 
 #define OPT_bool(name)  \
 void cre2_opt_##name(cre2_options *opt, int flag) {  \
-    OPT(opt)->set_##name(bool(flag));                \
+    TO_OPT(opt)->set_##name(bool(flag));             \
 }
 
 OPT_bool(posix_syntax)
@@ -33,14 +35,86 @@ OPT_bool(one_line)
 void cre2_opt_encoding(cre2_options *opt, enum Encoding enc) {
     switch (enc) {
         case EncodingUTF8:
-            OPT(opt)->set_encoding(RE2::Options::EncodingUTF8);
+            TO_OPT(opt)->set_encoding(RE2::Options::EncodingUTF8);
             break;
         case EncodingLatin1:
-            OPT(opt)->set_encoding(RE2::Options::EncodingLatin1);
+            TO_OPT(opt)->set_encoding(RE2::Options::EncodingLatin1);
             break;
     }
 }
 
 void cre2_opt_max_mem(cre2_options *opt, int m) {
-    OPT(opt)->set_max_mem(m);
+    TO_OPT(opt)->set_max_mem(m);
+}
+
+
+#define TO_RE2(re)       (reinterpret_cast<RE2 *>(re))
+#define TO_CONST_RE2(re) (reinterpret_cast<const RE2 *>(re))
+
+cre2 *cre2_new(const char *pattern, const cre2_options *opt) {
+    return reinterpret_cast<void*>(
+        new RE2(pattern, *reinterpret_cast<const RE2::Options *>(opt)));
+}
+
+void cre2_delete(cre2 *re) {
+    delete TO_RE2(re);
+}
+
+
+int cre2_error_code(const cre2 *re) {
+    return int(TO_CONST_RE2(re)->error_code());
+}
+
+char *cre2_error_string(const cre2 *re) {
+    return strdup(TO_CONST_RE2(re)->error().c_str());
+}
+
+char *cre2_error_arg(const cre2 *re) {
+    return strdup(TO_CONST_RE2(re)->error_arg().c_str());
+}
+
+int cre2_num_capturing_groups(const cre2 *re) {
+    return TO_CONST_RE2(re)->NumberOfCapturingGroups();
+}
+
+int cre2_program_size(const cre2 *re) {
+    return TO_CONST_RE2(re)->ProgramSize();
+}
+
+
+int cre2_match(
+    const cre2 *re
+  , const char *text
+  , int startpos
+  , int endpos
+  , enum Anchor anchor
+  , struct string_piece *match
+  , int nmatch) {
+
+    // FIXME: exceptions?
+    re2::StringPiece *match_re2 = new re2::StringPiece[nmatch];
+
+    RE2::Anchor anchor_re2;
+    switch (anchor) {
+        case UNANCHORED:
+            anchor_re2 = RE2::UNANCHORED;   break;
+        case ANCHOR_START:
+            anchor_re2 = RE2::ANCHOR_START; break;
+        case ANCHOR_BOTH:
+            anchor_re2 = RE2::ANCHOR_BOTH;  break;
+    }
+
+    bool ret = TO_CONST_RE2(re)
+        ->Match(text, startpos, endpos, anchor_re2, match_re2, nmatch);
+
+    if (ret) {
+        for (int i=0; i<nmatch; i++) {
+            match[i].data   = match_re2[i].data();
+            match[i].length = match_re2[i].length();
+        }
+    }
+
+    delete [] match_re2;
+
+    return int(ret);
 }
--- cbits/cre2.h
+++ cbits/cre2.h
@@ -26,6 +26,39 @@ void cre2_opt_encoding(cre2_options *opt, enum Encoding enc);
 void cre2_opt_max_mem(cre2_options *opt, int m);
 
 
+typedef void cre2;
+
+cre2 *cre2_new(const char *pattern, const cre2_options *opt);
+void cre2_delete(cre2 *re);
+
+int cre2_error_code(const cre2 *re);
+char *cre2_error_string(const cre2 *re);  // must free() string
+char *cre2_error_arg(const cre2 *re);     // must free() string
+int cre2_num_capturing_groups(const cre2 *re);
+int cre2_program_size(const cre2 *re);
+
+
+enum Anchor {
+    UNANCHORED,
+    ANCHOR_START,
+    ANCHOR_BOTH
+};
+
+struct string_piece {
+    const char *data;
+    int length;
+};
+
+int cre2_match(
+      const cre2 *re
+    , const char *text
+    , int startpos
+    , int endpos
+    , enum Anchor anchor
+    , struct string_piece *match
+    , int nmatch);
+
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
